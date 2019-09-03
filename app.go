254abe05524f2cf6b97fbb3b6c97e9b13f36f33e
Manuel Juan Martinez <dotjuan89@gmail.com>
2019-09-04 01:42:25 +0800

    Initial Commit

---
+++ app.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
